6bc50682cb035f6273feb25678873cd25e4994ea
Alizzie <79795653+Alizzie@users.noreply.github.com>
2021-04-08 16:48:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const openCloseSymbol = document.getElementsByClassName("openCloseSymbol")[0];
 const mobileToggleBackground = Array.from(document.getElementsByClassName("mobileToggleBackground"));
 const navbarLinks = Array.from(document.getElementsByClassName("navbarLinks"));
 const mobileToggleMenu = mobileToggleBackground.concat(navbarLinks);
+const creations = Array.from(document.getElementsByClassName("creation"));
 
 // Mobile Navigation Design
 openCloseSymbol.addEventListener("click", function(){
   openCloseSymbol.classList.toggle("closeIcon");
   mobileToggleMenu.forEach(element => element.classList.toggle("hidden"));
 })
+
+// Creations Redirection (for now back to start)
+
+creations.forEach(creation => creation.addEventListener("click", function(){
+  window.location.href = "#";
+}))
